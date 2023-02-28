84186ba6f223bcbe4824a58170fcfdf144915c9a
unknown <nggvanhieu@gmail.com>
2023-02-28 17:21:43 +0700

    update augard

--- src/test/test.module.ts
+++ src/test/test.module.ts
@@ -3,9 +3,10 @@ import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { TestController } from './test.controller';
 import { TestService } from './test.service';
 import { PassportModule } from '@nestjs/passport';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
   controllers: [TestController],
   providers: [TestService],
-  imports : [PassportModule]
+  imports : [AuthModule]
 })
 export class TestModule {}
